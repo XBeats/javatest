7b0504b464b8a3b9648027d03768c25211a89ddc
xbeats <851565228@qq.com>
2017-04-18 21:04:49 +0800

    添加char项目

---
+++ src/com/aitangba/test/chartest/CharTest.java
@@ -0,0 +1,16 @@
+package com.aitangba.test.chartest;
+
+/**
+ * Created by XBeats on 2017/1/2.
+ */
+public class CharTest {
+
+    public static void main(String args[]) {
+
+//        int num = Integer.valueOf('A');
+        int num = 'A';
+//        num  = new Integer('B');
+        System.out.println((char)(num + 1));
+
+    }
+}
